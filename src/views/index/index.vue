06cd9cec270c837d73c58415020074e07168216a
Zongxiang Han <hanzx@ebondmed.com>
2018-12-29 17:09:32 +0800

    首页更新

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -15,54 +15,60 @@
     <div class="w960">
       <img src="/static/imgs/long.jpg" style="vertical-align:middle">
     </div>
-    <div class="help w960">
-      <h4>用户注册与登录</h4>
-      <div class="help-detail">
+    <div class="w960 reglog">
+      <div class="left-content">
+        <h2><i class="left-border"></i>用户注册与登录</h2>
         <p>1、首先，您要先进行注册，在登陆按钮图标的下方，请点击它，即可进行注册。
         </p>
         <p>2、登陆，直接点击登陆/注册图标，输入您的用户名与密码即可进行登陆，登陆后方可发表留言、订购套餐等。
-
         </p>
         <p>3、找回密码，在您忘记了密码或者丢失了密码的情况下，请点击忘记密码，进行相应的操作，您将可以获得密码。
         </p>
       </div>
-      <h4>留言板的使用</h4>
-      <div class="help-detail">
-        <p>
-          当您进行了登陆后，点击留言板，即可进行留言，在留言过程中，请仔细填写留言信息。留言请遵守本国国家法律法规，请勿使用谩骂、诋毁等来进行对他人的人身攻击。
-        </p>
-        <p>
-          如果要投诉本酒吧的服务请利用站内邮件发邮件给管理员。详情请看“站内邮件的使用方法”。
-        </p>
+      <div class="right-content">
+        <img src="/static/imgs/poster1.jpg">
       </div>
-      <h4>会员推荐音乐</h4>
-      <div class="help-detail">
+    </div>
+    <div class="w960 recommend">
+      <div class="left-content">
+        <img src="/static/imgs/poster2.jpg">
+      </div>
+      <div class="right-content">
+        <h2><i class="left-border"></i>会员推荐歌曲</h2>
         <p>
-          会员推荐音乐主要是会员把好的音乐推荐给本酒吧。
+          会员推荐音乐主要是推荐优质音乐给本酒吧。
         </p>
         <p>
           每个会员能添加无限首歌！
         </p>
       </div>
-      <h4>站内邮件使用</h4>
-      <div class="help-detail">
-        <p>1、站内邮件只可以在站内发送和使用，不能发送站外邮件。</p>
-        <p>2、发送站内邮件必须知道对方的用户名 。</p>
+    </div>
+    <div class="w960 message">
+      <div class="left-content">
+        <h2><i class="left-border"></i>用户留言板</h2>
+        <p>
+          用户留言板主要用来收集顾客对酒吧服务的评价与改进建议等。
+        </p>
+        <p>
+          所有用户可以根据酒吧所提供服务公正的发表留言，以提升日后酒吧服务水平。
+        </p>
       </div>
-      <h4>站内邮件使用</h4>
-      <div class="help-detail">
-        <p>1、站内邮件只可以在站内发送和使用，不能发送站外邮件。</p>
-        <p>2、发送站内邮件必须知道对方的用户名 。</p>
+      <div class="right-content">
+        <img src="/static/imgs/poster3.jpg">
       </div>
-      <h4>站内邮件使用</h4>
-      <div class="help-detail">
-        <p>1、站内邮件只可以在站内发送和使用，不能发送站外邮件。</p>
-        <p>2、发送站内邮件必须知道对方的用户名 。</p>
+    </div>
+    <div class="w960 newscenter">
+      <div class="left-content">
+        <img src="/static/imgs/poster4.jpg">
       </div>
-      <h4>站内邮件使用</h4>
-      <div class="help-detail">
-        <p>1、站内邮件只可以在站内发送和使用，不能发送站外邮件。</p>
-        <p>2、发送站内邮件必须知道对方的用户名 。</p>
+      <div class="right-content">
+        <h2><i class="left-border"></i>新闻中心</h2>
+        <p>
+          新闻中心主要为使用者提供当前国际或国内要闻。
+        </p>
+        <p>
+          用户可自行根据喜好查看新闻的详情并发表相关评论。
+        </p>
       </div>
     </div>
     <div class="footer">
@@ -124,28 +130,54 @@ export default {
 img{
   border-radius: 2px;
 }
-.help{
-  h4{
-    height: 30px;
-    padding: 0 10px 0 10px;
-    line-height: 30px;
+.reglog, .message {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  line-height: 2;
+  h2 {
     font-weight: normal;
-    font-size: 14px;
-    background-color: #409eff;
-    color: #000;
-    border-bottom: 1px solid #cde99f;
   }
-  .help-detail{
-    background-color: #171B20;
-    color: #fff;
-    padding-left:10px;
-
-    p{
-      height: 45px;
-      font-size: 12px;
-      line-height: 45px;
+  .left-content {
+    width: 50%;
+    margin: auto 10px;
+  }
+  .right-content {
+    width: 50%;
+    img {
+      width: 100%;
     }
   }
+}
+.recommend, .newscenter {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  line-height: 2;
+  h2 {
+    font-weight: normal;
+  }
+  .left-content {
+    width: 50%;
+    img {
+      width: 100%;
+    }
+  }
+  .right-content {
+    width: 50%;
+    margin: auto 10px;
+    padding-left: 25px;
+    box-sizing: border-box;
+  }
+}
+.left-border {
+  display: inline-block;
+  width: 4px;
+  height: 30px;
+  background: #ff6700;
+  position: relative;
+  top: 6px;
+  margin-right: 15px;
 }
 .footer{
   background: url("/static/imgs/topnav_bg.gif") repeat-x center center;
