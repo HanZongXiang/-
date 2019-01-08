dd27116d5f5ab853e6ad284fe6a8adcbc87a17d1
Zongxiang Han <hanzx@ebondmed.com>
2019-01-08 17:49:20 +0800

    add comment

--- src/views/news/newsDetail.vue
+++ src/views/news/newsDetail.vue
@@ -8,34 +8,38 @@
         <div class="comment-title">评论</div>
         <div class="input-wrap">
           <img :src="this.$store.state.userInfo.avatar" class="avatar">
-          <input type="text" placeholder="输入评论...">
-          <el-button type="primary" size="mini">评论</el-button>
+          <input type="text" placeholder="输入评论..." v-model="commentContent" @keyup.enter="addComment">
+          <el-button type="primary" size="mini" @click="addComment">评论</el-button>
         </div>
         <div class="comment-content" style="margin-top: 10px;padding-left: 60px;">
           <div class="comment-item" v-for="(item, index) in comment" :key="index">
             <div class="left-avatar">
               <div class="avatar-wrap">
-                <img :src="item.avatar" class="avatar">
+                <img :src="item.user.avatar" class="avatar">
               </div>
             </div>
             <div class="right">
               <div class="right-content">
                 <div class="name">
-                  <span style="color: #333;font-size: 14px;">{{item.name}}</span>
+                  <span style="color: #333;font-size: 14px;">{{item.user.username}}</span>
                 </div>
                 <div class="comment-detail" style="color: #666;font-size: 13px;margin: 10px 0 15px;">
                   {{item.content}}
                 </div>
                 <div class="bottom">
-                  <div style="color: #8a9aa9;font-size: 13px;">{{item.time}}</div>
-                  <div style="font-size: 12px;color: #8a93a0;user-select: none;cursor: pointer;"><svg-icon icon-class="reply"></svg-icon> 回复</div>
+                  <div style="color: #8a9aa9;font-size: 13px;">{{item.createdTime}}</div>
+                  <div style="font-size: 12px;color: #8a93a0;user-select: none;cursor: pointer;" @click="reply(item)"><svg-icon icon-class="reply"></svg-icon> 回复</div>
+                </div>
+                <div style="display: flex;justify-content: space-between;margin-top: 10px;" v-if="item.editing">
+                  <el-input size="mini" style="width: 91%;" v-model="replyContent"></el-input>
+                  <el-button type="warning" size="mini" @click="submitReply(item)">提交</el-button>
                 </div>
               </div>
               <div class="reply-wrap" v-if="item.reply.length !== 0">
                 <div class="reply-content" v-for="(replyItem, idx) in item.reply" :key="idx">
                   <div class="reply-left">
                     <div class="reply-avatar">
-                      <img :src="replyItem.avatar" class="avatar">
+                      <img :src="item.user.avatar" class="avatar">
                     </div>
                   </div>
                   <div class="reply-right">
@@ -46,8 +50,12 @@
                       回复 <a style="color: #406599;">{{replyItem.responder}}</a>: {{replyItem.content}}
                     </div>
                     <div class="bottom">
-                      <div style="color: #8a9aa9;font-size: 13px;">{{replyItem.time}}</div>
-                      <div style="font-size: 12px;color: #8a93a0;user-select: none;cursor: pointer;"><svg-icon icon-class="reply"></svg-icon> 回复</div>
+                      <div style="color: #8a9aa9;font-size: 13px;">{{replyItem.createdTime}}</div>
+                      <div style="font-size: 12px;color: #8a93a0;user-select: none;cursor: pointer;" @click="replyInner(replyItem)"><svg-icon icon-class="reply"></svg-icon> 回复</div>
+                    </div>
+                    <div style="display: flex;justify-content: space-between;margin-top: 10px;" v-if="replyItem.editing">
+                      <el-input size="mini" style="width: 89%;" v-model="replyContent"></el-input>
+                      <el-button type="warning" size="mini" @click="innerSubmit(item, replyItem)">提交</el-button>
                     </div>
                   </div>
                 </div>
@@ -67,59 +75,11 @@ export default {
   name:'',
   data() {
     return {
+      replyContent: '',
+      showReply: false,
       details:'',
-      comment: [
-        {
-          name: '别来无恙',
-          time: '3分钟前',
-          content: '讲得非常好',
-          avatar: 'http://pbl.yaojunrong.com/FvRdKNv1h5d0rO3ypYh8R-Foy9gH',
-          reply: [
-            {
-              responder: '别来无恙',
-              reviewer: '路人甲',
-              avatar: 'http://pbl.yaojunrong.com/FnKsUTjxTcJLayg751O5v8Nt_ZtL',
-              time: '两分钟前',
-              content: '没毛病老铁'
-            },
-            {
-              responder: '路人甲',
-              reviewer: '路人乙',
-              avatar: 'http://pbl.yaojunrong.com/FnKsUTjxTcJLayg751O5v8Nt_ZtL',
-              time: '两分钟前',
-              content: '双击666'
-            },
-            {
-              responder: '路人乙',
-              reviewer: '路人丙',
-              avatar: 'http://pbl.yaojunrong.com/FnKsUTjxTcJLayg751O5v8Nt_ZtL',
-              time: '两分钟前',
-              content: '老铁牛逼'
-            }
-          ]
-        },
-        {
-          name: '路人甲乙',
-          time: '3分钟前',
-          content: '讲得非常好1',
-          avatar: 'http://pbl.yaojunrong.com/FvRdKNv1h5d0rO3ypYh8R-Foy9gH',
-          reply: []
-        },
-        {
-          name: '南柯一梦',
-          time: '4分钟前',
-          content: '讲得非常好2',
-          avatar: 'http://pbl.yaojunrong.com/FvRdKNv1h5d0rO3ypYh8R-Foy9gH',
-          reply: []
-        },
-        {
-          name: '别来无恙a',
-          time: '6分钟前',
-          content: '讲得非常好3',
-          avatar: 'http://pbl.yaojunrong.com/FvRdKNv1h5d0rO3ypYh8R-Foy9gH',
-          reply: []
-        },
-      ]
+      comment: [],
+      commentContent: ''
     }
   },
   components: {
@@ -133,10 +93,67 @@ export default {
           this.details = res.data
         }
       })
+    },
+    getComment () {
+      this.$axios.get(`/comment/${this.$route.params.id}`).then(res => {
+        this.comment = res.data
+      })
+    },
+    addComment () {
+      if (this.commentContent === '' || this.commentContent == undefined) {
+        this.$message.info('请输入评论内容')
+        return
+      }
+      this.$axios.post('/comment', {
+        content: this.commentContent,
+        newsId: this.$route.params.id
+      }).then(res => {
+        if (res.code === 200) {
+          this.$message.success(res.msg)
+        }
+      })
+      this.commentContent = ''
+      window.location.reload()
+    },
+    reply (item) {
+      item.editing = !item.editing
+    },
+    replyInner (replyItem) {
+      replyItem.editing = !replyItem.editing
+    },
+    submitReply (item) {
+      let responder = item.user._id
+      let content = this.replyContent
+      this.$axios.post('/reply', {responder,content, commentId: item._id}).then(res => {
+        if (res.code === 200) {
+          this.$message.success(res.msg)
+        }
+      })
+      this.replyContent = ''
+      this.getComment()
+    },
+    innerSubmit (item, replyItem) {
+      this.$axios.post('/reply', {responder: replyItem.reviewer,content: this.replyContent, commentId: item._id}).then(res => {
+        if (res.code === 200) {
+          this.$message.success(res.msg)
+        }
+      })
     }
+    // getReviewer (item) {
+    //   this.$axios.get(`/user/${item.reviewer}`).then(res => {
+    //     return item.reviewerInfo = res.data
+    //   })
+    // }
   },
   created() {
     this.getDetails()
+    this.getComment()
+    this.$axios.get('/order').then(res => {
+      console.log(res)
+    })
+  },
+  computed: {
+
   }
 }
 </script>
